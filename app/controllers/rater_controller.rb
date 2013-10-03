82372748977cc616841b0a220e73cd1fc1aa79c7
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-03 14:57:53 +0100

    custom rating

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -1,14 +1,10 @@
 class RaterController < ApplicationController 
   
   def create                                  
-    if current_user.present?
-      obj = eval "#{params[:klass]}.find(#{params[:id]})"     
-      if params[:dimension].present?
-        obj.rate params[:score].to_i, current_user.id, "#{params[:dimension]}"       
-      else
-        obj.rate params[:score].to_i, current_user.id 
-      end
-      
+    if current_user
+      article = Article.find(params[:id])     
+      params[:dimension] ||= 5
+      article.rate params[:score].to_i, current_user.id, "#{params[:dimension]}"       
       render :json => true 
     else
       render :json => false        
