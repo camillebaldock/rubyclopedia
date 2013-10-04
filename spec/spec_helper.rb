8f396af6470d3fbf25c6e2f8bd7bad528420ff71
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-04 14:13:28 +0100

    Coveralls before requirements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
+require 'coveralls'
+Coveralls.wear!
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
-
-require 'coveralls'
-Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
