cba3d272d85285f8603247c9a75952f9f5a9e674
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-04 12:11:35 +0100

    remove index names

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,11 +1,1 @@
 ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
-
-if ENV['BONSAI_INDEX_URL']
-  bonsai_uri = URI.parse(ENV['BONSAI_INDEX_URL'])
-  Tire.configure do
-    url "http://index.bonsai.io"
-  end
-  BONSAI_INDEX_NAME = bonsai_uri.path[1..-1]
-else
-  BONSAI_INDEX_NAME = "rubyclopedia"
-end
