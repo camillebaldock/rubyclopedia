0ae3eef3d96b11e83d4927585815cb0010d5fd79
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-04 13:08:25 +0100

    environment error

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Rubyclopedia::Application.initialize!
-
-config.action_mailer.default_url_options = { :host => 'localhost' }
