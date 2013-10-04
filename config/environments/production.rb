b4231b95205390c629e2f6a130a9857c85fc06ac
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-04 13:04:22 +0100

    Action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rubyclopedia::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => 'rubyclopedia.com' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
