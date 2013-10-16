f7f3137d6febe7215005ce6ca5cd15b1252a8738
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-16 14:06:26 +0100

    Superuser flag for users

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(:version => 20131003003554855) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "username"
+    t.boolean  "superuser"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
