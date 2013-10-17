b9b49ccc60634171747c61c3215ba62f0b5d3428
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-17 02:03:19 +0100

    Fix DB migration Travis

--- db/migrate/20131016221121_clean_up_database.rb
+++ db/migrate/20131016221121_clean_up_database.rb
@@ -1,8 +1,6 @@
 class CleanUpDatabase < ActiveRecord::Migration
   def change
     drop_table :tags
-    drop_table :rates
-    drop_table :rating_caches
     add_index :users, :username
     remove_column :articles, :duration_seconds
     remove_column :submissions, :reason
