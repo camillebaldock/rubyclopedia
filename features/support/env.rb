c1538bb410adc93c9a113f3790f83a5f12b9f560
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-17 23:09:01 +0100

    Merging coverage results in coveralls

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,7 @@
 # files.
 require 'cucumber/rails'
 require 'coveralls'
-Coveralls.wear! 
+Coveralls.wear_merged! 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
