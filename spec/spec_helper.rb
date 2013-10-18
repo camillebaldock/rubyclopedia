cf55373c95b644d5e3a54bbbc14b5142ad95b641
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-18 20:39:04 +0100

    Rails-specific coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'coveralls'
-Coveralls.wear_merged!
+Coveralls.wear_merged!('rails')
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
