08c6214bd8fda50ca3ca0683087b281f4ee401c1
Camille Baldock <camille@camillebaldock.co.uk>
2013-10-19 00:42:03 +0100

    Copyright

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,20 @@
+=begin
+Copyright (C) 2013 Camille Baldock
+Rubyclopedia is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+Rubyclopedia is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+=end
 class SubmissionsController < ApplicationController
   before_filter :authenticate_user!
-  # GET /submissions/new
-  # GET /submissions/new.json
+
   def new
     @submission = Submission.new
 
@@ -11,8 +24,6 @@ class SubmissionsController < ApplicationController
     end
   end
 
-  # POST /submissions
-  # POST /submissions.json
   def create
     @submission = Submission.new(params[:submission])
     @submission.status = "New"
